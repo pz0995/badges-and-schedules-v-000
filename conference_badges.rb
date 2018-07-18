d15ec8672f6879ff9a170b442f6315c5d81808ea
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 06:31:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def batch_badge_creator(names, messages)
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+
+def assign_rooms
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = ["one", "two", "three", "four", "five", "six", "seven"]
+
+
+names.each do |item|
+    puts item
+ "Hello, #{speaker}! You'll be assigned to room #{room}!"
+  end
+end
