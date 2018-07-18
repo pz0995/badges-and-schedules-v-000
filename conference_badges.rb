3c8cd4c6826354971c3e9149d6427cb119da4983
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 13:15:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,34 @@
-def batch_badge_creator(names, messages)
-speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-
-
-def assign_rooms
-speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-room = ["one", "two", "three", "four", "five", "six", "seven"]
-
-
-names.each do |item|
-    puts item
- "Hello, #{speaker}! You'll be assigned to room #{room}!"
+def batch_badge_creator(speaker, attendee)
+    speaker = (["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+    speaker = speaker.each do |attendee|
+    return "Hello, my name is #{attendee}."
+    end
   end
+
+def assign_rooms(speaker, room)
+  speaker = (["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+  room = (["one", "two", "three", "four", "five", "six", "seven"])
+
+    speaker.each do |speak|
+      room.each do|roo|
+  return "Hello, #{speak}! You'll be assigned to room #{roo}!"
+
+  end
+end
+
+def badge_maker(name)
+  name = (["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+  name.each do |speak|
+  return "Hello, my name is #{speak}."
+  end
+end
+
+
+
+def printer(speaker, room)
+    speaker.each do |speak|
+      room.each do |aroom|
+      puts "#{speaker}"
+      puts "#{aroom}"
+end
 end
